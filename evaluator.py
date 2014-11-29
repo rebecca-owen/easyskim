1a488db110e26187dedd6fb85cd2c62f1826529d
Josh Cai <yuanshi419@gmail.com>
2014-11-29 22:18:29 +0000

    deleted extra parentheses

--- evaluator.py
+++ evaluator.py
@@ -4,7 +4,7 @@ def getBibliography(pdfText):
     possibilities = "(BIBLIOGRAPHY)|(Bibliography)|(References)|(REFERENCES)"
     from re import sub
     #bibliography = re.sub("".join([".*",possibilities]),"",pdfText))
-    return sub("".join([".*",possibilities]),"",pdfText))
+    return sub("".join([".*",possibilities]),"",pdfText)
 def authorCounter(pdfText):
     pass
 def paperCounter(pdfText):
