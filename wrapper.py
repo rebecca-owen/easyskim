8f6deba874ba5b988b27299a5ba17e4834e0cfcb
Keller Scholl <keller.scholl@gmail.com>
2014-11-29 19:27:06 +0000

    fixed silly mistakes on my part.

--- wrapper.py
+++ wrapper.py
@@ -1,8 +1,8 @@
 from extract import extract
 from evaluator import evaluator, authorCounter, paperCounter
-def textChanger(pdfText, mostAuthor="", mostPaper="",extractOptions="nltkalchemy",devMode=false):
-	""""Takes the semi-cleaned text of a pdf and extracts the desired portions. Output in markdown suitable for display on the website."""
-	if mostAuthor:
+def textChanger(pdfText, mostAuthor="", mostPaper="",extractOptions="nltkalchemy",devMode=False):
+    """"Takes the semi-cleaned text of a pdf and extracts the desired portions. Output in markdown suitable for display on the website."""
+    if mostAuthor:
         mostAuthor = evaluator(authorCounter(pdfText))
     if mostPaper:
         mostPaper = evaluator(paperCounter(pdfText))
@@ -10,6 +10,6 @@ def textChanger(pdfText, mostAuthor="", mostPaper="",extractOptions="nltkalchemy
     #return ''.join([mostAuthor,mostPaper,ex])
     return pdfText
 if __name__=="__main__":
-	import codecs
-	cleanText=codecs.open("sample.txt",encoding="utf-8").read()
-	textChanger()
+    import codecs
+    cleanText=codecs.open("sample.txt",encoding="utf-8").read()
+    #textChanger()
