00866eda067bba52a144db0fc32017ff66be9760
Keller Scholl <keller.scholl@gmail.com>
2014-11-30 11:04:05 +0000

    fixed the '', u'' problem

--- wrapper.py
+++ wrapper.py
@@ -11,6 +11,7 @@ def textChanger(pdfText, mostAuthor="", mostPaper="",extractOptions=["nltkalchem
         mostPaper = evaluator(paperCounter(pdfText))
     ex = extract(pdfText,extractOptions)
     ex=re.sub("\\\\n"," ",ex)
+    ex=re.sub('", u"','',ex)
     return ex
 if __name__=="__main__":
     import codecs
